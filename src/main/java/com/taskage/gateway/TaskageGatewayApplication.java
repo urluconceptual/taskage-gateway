939dba71c3eab8d02d1ce2ddd83cfad32f63c0e3
Alexandra-Diana Ciocan <ciocan_alexandra16@yahoo.com>
2024-05-13 19:47:06 +0300

    remove useless directory

--- src/main/java/com/taskage/gateway/TaskageGatewayApplication.java
+++ src/main/java/com/taskage/gateway/TaskageGatewayApplication.java
@@ -1,4 +1,4 @@
-package com.taskage.gateway.taskagegateway;
+package com.taskage.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
